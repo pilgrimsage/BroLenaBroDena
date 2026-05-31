6f2579bc800bd2c32b7c896c20e54142eb0b8654
pilgrimsage <sksmukhia.dev@gmail.com>
2026-05-31 21:21:13 +0530

    fix assets for build

--- backend/resources/views/spa.blade.php
+++ backend/resources/views/spa.blade.php
@@ -5,8 +5,8 @@
     <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>frontend</title>
-    <script type="module" crossorigin src="/assets/index-tpUmiBYT.js"></script>
-    <link rel="stylesheet" crossorigin href="/assets/index-Day-NZBE.css">
+    <script type="module" crossorigin src="assets/js/index-tpUmiBYT.js"></script>
+    <link rel="stylesheet" crossorigin href="assets/css/index-Day-NZBE.css">
   </head>
   <body>
     <div id="root"></div>
